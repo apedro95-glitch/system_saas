104eaefb18933db687eca5d187f5e347929cf2d8
apedro95-glitch <apedro.95@icloud.com>
2026-04-26 16:27:06 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE = 'topbrs-multicla-auth-v1';
-const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/img/auth-bg.svg','./assets/icons/app-icon.svg','./assets/icons/clan.svg'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
